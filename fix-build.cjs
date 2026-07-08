5190ba74e6144d6ae51deb41b74969a70c2d2ea5
jiao1232022 <jiao1232022@163.com>
2026-07-08 15:16:18 +0800

    添加自动构建修复脚本 fix-build.cjs

---
+++ fix-build.cjs
@@ -0,0 +1,8 @@
+﻿const fs = require("fs");
+const path = require("path");
+const file = path.join(__dirname, "docs", "index.html");
+let c = fs.readFileSync(file, "utf-8");
+c = c.replace('<script type="module" crossorigin>', "<script>");
+c = c.replace(/ crossorigin/g, "");
+fs.writeFileSync(file, c);
+console.log("✅ Fixed: removed type=module and crossorigin");
